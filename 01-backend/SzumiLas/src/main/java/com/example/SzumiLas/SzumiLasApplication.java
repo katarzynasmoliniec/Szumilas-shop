a3018b4b3bc86fd471c96c845dd8a985101f9565
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2024-04-10 11:17:39 +0200

    V1_Create starter files of SzumiLas_shop

---
+++ 01-backend/SzumiLas/src/main/java/com/example/SzumiLas/SzumiLasApplication.java
@@ -0,0 +1,13 @@
+package com.example.SzumiLas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzumiLasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzumiLasApplication.class, args);
+	}
+
+}
---
+++ 01-backend/SzumiLas/src/test/java/com/example/SzumiLas/SzumiLasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SzumiLas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzumiLasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
