8fea7583d19fa9a31627548a50c3785994a5d26b
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2024-04-11 19:54:06 +0200

    V9_CartDetails- increment, decrement, remove with cart

--- 02-frontend/szumilas-shop/src/app/app.module.ts
+++ 02-frontend/szumilas-shop/src/app/app.module.ts
@@ -12,8 +12,10 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
+  {path: 'cart-datails', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id/:name', component: ProductListComponent},
@@ -32,7 +34,8 @@ const routes: Routes = [
     SearchComponent,
     ProductDetailsComponent,
     CheckoutComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- 02-frontend/szumilas-shop/src/app/services/cart.service.ts
+++ 02-frontend/szumilas-shop/src/app/services/cart.service.ts
@@ -40,6 +40,28 @@ export class CartService {
      this.computeCartTotals();
   }
 
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if(theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    //get index of itemin array 
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id == theCartItem.id);
+    
+    // if found remove the item from arra at the given index
+    if(itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
+    
+  }
+
   computeCartTotals() {
 
     let totalPriceValue: number = 0;
