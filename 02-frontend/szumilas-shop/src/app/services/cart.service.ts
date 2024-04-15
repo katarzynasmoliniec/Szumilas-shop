ee32f37956a793b8156ab0bc9171978ac08525ba
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2024-04-15 13:01:15 +0200

    V16_Angular->remember cart-> cartService

--- 02-frontend/szumilas-shop/src/app/services/cart.service.ts
+++ 02-frontend/szumilas-shop/src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../overall/cart-item';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,20 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  storage: Storage = sessionStorage;
+
+  constructor() { 
+
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if(data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data is read from storage
+      this.computeCartTotals();
+    }
+  }
 
   addToCart(theCartItem: CartItem) {
     // chcec if we alredy have the item in our cart
@@ -75,5 +89,12 @@ export class CartService {
     // publish the new values ... all subscribers will receive the new data
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 }
